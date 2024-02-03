b71f3ef132f0013b84cb455660ec3b4efed0b43a
Александр <121234051+Alexandr-Bell@users.noreply.github.com>
2024-02-03 22:40:18 +0300

    Update lesson_2_task_4.py

--- lesson_2_task_4.py
+++ lesson_2_task_4.py
@@ -1,17 +1,18 @@
-def fizz_buzz(n):
-    for x in range( n + 1):
+
+
+def  fizz_buzz(n):
+ for x in range( n + 1):
          if x % 3 == 0 and  x % 5 == 0:
             print("FizzBuzz")
-      
+   
          elif x % 3 == 0:
             print("Fizz")
 
          elif x % 5 == 0:
             print("Buzz") 
-      
-      
          else: 
-          print(x)
-    
+            print(x)
 
-fizz_buzz(77) 
+input_value = int(input("Введите число:" ))
+
+fizz_buzz(input_value)
