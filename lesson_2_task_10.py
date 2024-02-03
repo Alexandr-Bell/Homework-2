d3bacca69acc0ec212c1a8647efbcf266f63433a
Александр <121234051+Alexandr-Bell@users.noreply.github.com>
2024-02-03 22:28:24 +0300

    Update lesson_2_task_10.py

--- lesson_2_task_10.py
+++ lesson_2_task_10.py
@@ -1,7 +1,10 @@
 def bank(X, Y):
-    amount = X * (1 + 0.10)**Y
+    amount = X * (1 + 0.14)**Y
     return amount
-initial_deposit = 100000
-years = 10
+x = float(input("What amount would you like to deposit? "))
+y = int(input("How many years would you like to keep your investment for? "))
+
+initial_deposit = x
+years = y
 result = bank(initial_deposit, years)
-print("Сумма на счету через", years, "лет будет:", result)
+print(f"Сумма на счету через", years, "лет будет:", result)
